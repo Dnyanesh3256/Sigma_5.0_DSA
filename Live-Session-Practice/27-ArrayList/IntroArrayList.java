2308c881c89b05a104054453729c5d26c917d641
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-02-18 22:58:12 +0530

    18/02/2025

---
+++ Live-Session-Practice/27-ArrayList/IntroArrayList.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class IntroArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println(list.size());
+        list.add(5);
+        System.out.println(list.size());
+    }
+}
