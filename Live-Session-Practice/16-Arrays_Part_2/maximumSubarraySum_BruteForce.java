2ece4f8364907089df96816c48fa6b1011f2f71e
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-02-18 17:47:13 +0530

    18/02/2025

--- Live-Session-Practice/16-Arrays_Part_2/maximumSubarraySum_BruteForce.java
+++ Live-Session-Practice/16-Arrays_Part_2/maximumSubarraySum_BruteForce.java
@@ -1,66 +1,28 @@
-import java.util.Scanner;
-
 public class maximumSubarraySum_BruteForce {
     public static void subArrSum(int arr[]){
-        int sas = 0;
-        int tsas = 0;
-
+        int currSum;
         int maxSum = Integer.MIN_VALUE;
-        int minSum = Integer.MAX_VALUE;
 
         for(int i=0;i<arr.length;i++){
             int start = i;
             for(int j=i;j<arr.length;j++){
                 int end = j;
+                currSum = 0;
                 for(int k=start;k<=end;k++){
-                    sas += arr[k];
+                    currSum += arr[k];
                 }
-                System.out.println("Subarray Sum = " + sas);
-                tsas += sas;
-
-                maxSum = Math.max(maxSum, sas);
-                minSum = Math.min(minSum, sas);
-            }
+                if(currSum > maxSum){
+                    maxSum = currSum;
+                }
+                System.out.println("Subarray Sum = " + currSum);
+            }     
         }
-        
-        System.out.println("Total Subarray Sum = " + tsas);
-        System.out.println("Maximum Sum in Subarray = " + maxSum);
-        System.out.println("Minimum Sum in Subarray = " + minSum);
-    }
 
-    public static void printArr(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
-
-    public static void inputArr(int arr[]){
-        Scanner sc = new Scanner(System.in);
-        for(int i=0;i<arr.length;i++){
-            System.out.println("Enter element at index : " + i);
-            arr[i] = sc.nextInt();
-        }
-        System.out.println();
-
-        sc.close();
+        System.out.println("Maximum Sum = " + maxSum);
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter array size : ");
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-
-        System.out.println("Enter elements in array : ");
-        inputArr(arr);
-
-        System.out.println("Entered array is : ");
-        printArr(arr);
-
-        System.out.println("Subarray Sums are : ");
+        int arr[] = {2,4,6,8,10};
         subArrSum(arr);
-
-        sc.close();
     }
 }
